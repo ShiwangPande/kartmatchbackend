b126fb72748d3e71c1f8300d771279f66b2ac073
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2024-06-02 23:43:24 +0530

    first comment

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Vendor = require('./vendorModel');
-const cors = require('cors'); // Import the cors middleware
+const cors = require('cors');
 const fs = require('fs');
-const path = require('path'); // Import the path module
+const path = require('path');
 
 const app = express();
 const port = 3000;
@@ -11,8 +11,16 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
+let cachedData = null; // Variable to cache the data
+
 // Define API route to serve data.json
 app.get('/vendors', (req, res) => {
+    // If data is already cached, send it directly
+    if (cachedData) {
+        res.json(cachedData);
+        return;
+    }
+
     const dataFilePath = path.join(__dirname, 'data.json');
     fs.readFile(dataFilePath, 'utf8', (err, data) => {
         if (err) {
@@ -21,6 +29,10 @@ app.get('/vendors', (req, res) => {
             return;
         }
         const vendorData = JSON.parse(data).vendorData;
+
+        // Cache the data for future requests
+        cachedData = vendorData;
+
         res.json(vendorData);
     });
 });
