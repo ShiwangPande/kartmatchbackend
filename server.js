3a89574ef5bd87ebce0ceb93cb69f7d9a4aa4a25
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2024-07-11 00:20:58 +0530

    config

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-// MongoDB Connection
 const uri = "mongodb+srv://shiwang:shiwang@cluster0.ytjenqf.mongodb.net/kartmatch?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+
 });
 
 let db;
 let vendorData;
-let usersCollection;
-let commentsCollection;
 
 // Function to connect to the MongoDB server
 async function connectToDatabase() {
@@ -30,11 +30,8 @@ async function connectToDatabase() {
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
         db = client.db("kartmatch");
-        usersCollection = db.collection('users');
-        commentsCollection = db.collection('comments');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
-        process.exit(1); // Exit process if unable to connect to MongoDB
     }
 }
 
@@ -52,106 +49,34 @@ function loadVendorData() {
 
 // Connect to the database and load JSON data before starting the server
 async function initializeServer() {
-    try {
-        await connectToDatabase();
-        loadVendorData();
-    } catch (error) {
-        console.error('Error initializing server:', error);
-        process.exit(1); // Exit process if server initialization fails
-    }
+    await connectToDatabase();
+    loadVendorData();
 }
 
 initializeServer();
 
-// Middleware to check if MongoDB is connected before handling requests
-function checkDatabaseConnection(req, res, next) {
-    if (!db) {
-        res.status(500).json({ error: 'Database connection error' });
-    } else {
-        next();
-    }
-}
-
-// Endpoint to handle user consent status
-app.post('/api/users/:userId/consent', checkDatabaseConnection, async (req, res) => {
-    try {
-        const userId = req.params.userId;
-        const { consent } = req.body;
-
-        const result = await usersCollection.updateOne(
-            { _id: ObjectId(userId) },
-            { $set: { consentGiven: consent } }
-        );
-
-        res.status(200).json({ message: 'Consent updated successfully' });
-    } catch (error) {
-        console.error('Error updating user consent:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
-// Endpoint to handle safety reminder agreement
-// Endpoint to handle safety reminder agreement
-app.post('/api/users/:userId/safetyReminderAgree', async (req, res) => {
-    try {
-        const userId = req.params.userId;
-        if (!userId) {
-            return res.status(400).json({ error: 'Missing userId' });
-        }
-
-        await usersCollection.updateOne(
-            { _id: ObjectId(userId) },
-            { $set: { safetyReminderAgreed: true } }
-        );
-
-        res.status(200).json({ message: 'Safety reminder agreed successfully' });
-    } catch (error) {
-        console.error('Error agreeing to safety reminder:', error); // Log detailed error message
-        res.status(500).json({ error: 'Internal Server Error' }); // Return 500 status with generic error message
-    }
-});
-
-
-
-// Endpoint to handle parental consent
-app.post('/api/users/:userId/parentalConsent', checkDatabaseConnection, async (req, res) => {
-    try {
-        const userId = req.params.userId;
-        const { consentGiven } = req.body;
-        await usersCollection.updateOne(
-            { _id: ObjectId(userId) },
-            { $set: { parentalConsentGiven: consentGiven } }
-        );
-        res.status(200).json({ message: 'Parental consent given successfully' });
-    } catch (error) {
-        console.error('Error giving parental consent:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
-// Get vendor data endpoint
 app.get('/vendors', (req, res) => {
     if (!vendorData) {
-        res.status(500).json({ error: 'Vendor data not available' });
+        res.status(500).send('Internal Server Error');
         return;
     }
     res.json(vendorData);
 });
 
-// Get comments for a vendor endpoint
-app.get('/api/vendors/:vendorId/comments', checkDatabaseConnection, async (req, res) => {
+app.get('/api/vendors/:vendorId/comments', async (req, res) => {
     try {
+        const commentsCollection = db.collection('comments');
         const comments = await commentsCollection.find({ vendorId: req.params.vendorId }).toArray();
         res.json(comments);
     } catch (error) {
         console.error('Error fetching comments:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).send('Internal Server Error');
     }
 });
 
-// Add comment for a vendor endpoint
-app.post('/api/vendors/:vendorId/comments', checkDatabaseConnection, async (req, res) => {
+app.post('/api/vendors/:vendorId/comments', async (req, res) => {
     try {
+        const commentsCollection = db.collection('comments');
         const newComment = {
             vendorId: req.params.vendorId,
             comment: req.body.comment,
@@ -163,26 +88,25 @@ app.post('/api/vendors/:vendorId/comments', checkDatabaseConnection, async (req,
         res.status(201).json(result.ops[0]);
     } catch (error) {
         console.error('Error adding comment:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).send('Internal Server Error');
     }
 });
 
-// Delete comment endpoint
-app.delete('/api/comments/:commentId', checkDatabaseConnection, async (req, res) => {
+app.delete('/api/comments/:commentId', async (req, res) => {
     try {
+        const commentsCollection = db.collection('comments');
         const result = await commentsCollection.deleteOne({ _id: new ObjectId(req.params.commentId) });
         if (result.deletedCount === 1) {
             res.status(200).json({ message: 'Comment deleted successfully' });
         } else {
-            res.status(404).json({ error: 'Comment not found' });
+            res.status(404).json({ message: 'Comment not found' });
         }
     } catch (error) {
         console.error('Error deleting comment:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).send('Internal Server Error');
     }
 });
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
