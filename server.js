08486054ecb2748c2dc87fbe325ac4857b0b2568
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2024-07-27 17:00:47 +0530

    updated uri

--- server.js
+++ server.js
@@ -10,15 +10,14 @@ const port = 5000;
 app.use(express.json());
 app.use(cors());
 
-const uri = "mongodb+srv://kartmatchapp:kartmatchapp@cluster0.b0qfxiq.mongodb.net//kartmatch?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://kartmatchapp:kartmatchapp@ac-thjgwvf-shard-00-02.b0qfxiq.mongodb.net/kartmatch?authSource=admin&replicaSet=atlas-i4eib8-shard-0&retryWrites=true&w=majority&appName=Cluster0&ssl=true";
 const client = new MongoClient(uri, {
-    useUnifiedTopology: true,
+    useUnifiedTopology: true, // Use the new Server Discover and Monitoring engine
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
     }
-
 });
 
 let db;
@@ -33,6 +32,7 @@ async function connectToDatabase() {
         db = client.db("kartmatch");
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
+        process.exit(1); // Exit the process with failure
     }
 }
 
